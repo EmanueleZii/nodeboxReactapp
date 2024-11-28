0cbc8245cdc0042bd5147d6cf987066474bf0003
unknown <zignale92@gmail.com>
2024-11-28 17:32:44 +0100

    openwindow

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 const faqs = [
@@ -18,15 +19,42 @@ const faqs = [
   }
 ];
 
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+     <Accordition data={faqs} />
     </div>
   );
 }
 
-export default App;
+function Accordition({data})
+{
+  const [isOpen, setisOpen] = useState(false);
+  return (
+   <div className='accordion'> 
+    {
+    data.map(
+      (el, i) => 
+      <AccorditionItem title={el.title} text={el.text} num={i} key={el.title}/>
+     )}
+   </div>
+  );
+}
+
+function AccorditionItem({num, title, text})
+{
+  const [isOpen, setisOpen] = useState(false);
+
+  function handleToggle() {
+    setisOpen((isOpen) => !isOpen);
+  }
+
+  return (
+  <div className={`item ${isOpen ? "open":""}`} onClick={handleToggle}>
+    <p className='number'>{num < 9 ? `0${num + 1}` : num + 1}</p>
+    <p className='text'>{title}</p>
+    <p className='icon'>{isOpen ? '-':'+'}</p>
+    {isOpen && <div className='content-box'>{text}</div>}
+  </div>
+  );
+}
